a3ebac9bfa50d09017cda8734d689efd637eee29
Yandhi <15877410869@163.com>
2022-02-07 00:51:09 +0800

    ddos section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,11 +7,35 @@ export const Home = () => {
     <div className='home'>
       <header style={{backgroundImage: `url(${header})`}}/>
       <main>
-        <section className="section1"></section>
-        <section className="section2"></section>
-        <section className="section3"></section>
-        <section className="section4"></section>
-        <section className="section5"></section>
+        <section className="section1">
+          <div className="ddos">
+            <h2>今日DDOS攻击态势</h2>
+            <div className="chart"></div>
+          </div>
+        </section>
+        <section className="section2">
+          <div className="perception">
+            <h2>网络态势整体感知</h2>
+          </div>
+          <div className="warning">
+            <h2>红色预警</h2>
+          </div>
+        </section>
+        <section className="section3">
+          <div className="web">
+            <h2>今日WEB攻击姿势</h2>
+          </div>
+        </section>
+        <section className="section4">
+          <div className="net">
+            <h2>今日网络攻击姿势</h2>
+          </div>
+        </section>
+        <section className="section5">
+          <div className="host">
+            <h2>今日主机攻击姿势</h2>
+          </div>
+        </section>
       </main>
     </div>
   );
